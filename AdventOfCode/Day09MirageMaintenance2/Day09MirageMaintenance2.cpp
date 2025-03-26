11e5bdb305b0305fc1627e553c103904f7577e1b
Kenneth Glimberg Christensen <kenneth.glimberg.christensen@gmail.com>
2025-03-26 12:47:10 +0100

    Finish solving day 9 part 2

---
+++ AdventOfCode/Day09MirageMaintenance2/Day09MirageMaintenance2.cpp
@@ -0,0 +1,90 @@
+// Day09MirageMaintenance2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#if defined(__GNUC__) ||defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wc++98-compat"
+#pragma GCC diagnostic ignored "-Wc++98-compat-pedantic"
+#endif
+
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	std::string line;
+	long results = 0;
+
+	std::ifstream inputfile("input.txt");
+
+	if (!inputfile.is_open()) { std::cerr << "Cannot open file!\n"; return 0; }
+
+	while (std::getline(inputfile, line)) {
+
+		std::istringstream iss(line);
+		std::vector<long long> numbers{ std::istream_iterator<long long>(iss), std::istream_iterator<long long>() };
+
+
+		size_t step = 0;
+
+		long long prev = 0;
+		std::vector<long long> leftmost;
+		std::string o = "";
+
+		while (std::all_of(numbers.begin()+static_cast<int>(step), numbers.end(), [](const long long& i) { return i == 0LL; }) != true && step < numbers.size()) {
+
+			for (size_t idx = numbers.size() - 1; idx > step; idx--) {
+				if (idx == numbers.size() - 1) {
+					leftmost.emplace_back(numbers[step]);
+				}
+				numbers[idx] -= numbers[idx - 1];
+			}
+
+			step++;
+		}
+
+		for (int idx = static_cast<int>(leftmost.size()) - 1; idx >= 0; idx--) {
+			prev = leftmost[static_cast<size_t>(idx)] - prev;
+
+		}
+
+		results += prev;
+	}
+
+
+	inputfile.close();
+
+	std::cout << "Results: " << results << "\n";
+
+	auto stop = std::chrono::high_resolution_clock::now();
+
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+	std::cout << "Time taken by function: "
+		<< duration.count() << " microseconds\n";
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
